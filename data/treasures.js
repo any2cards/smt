9afccaa735fcc5cffaeffdeb41f74325a4090e10
any2cards <any2cards@yahoo.com>
2021-06-10 10:12:08 -0700

    Update treasures.js

--- data/treasures.js
+++ data/treasures.js
@@ -72,158 +72,152 @@
     "xws": "deadmanschest"
   },
   {
-    "name": "Dead Man’s Chest",
-    "points": 12,
-    "image": "treasures/dead-mans-chest.png",
-    "xws": "deadmanschest"
-  },
-  {
     "name": "Drake's Rapier",
-    "points": 13,
+    "points": 12,
     "image": "treasures/drakes-rapier.png",
     "xws": "drakesrapier"
   },
   {
     "name": "Dutch Flag",
-    "points": 14,
+    "points": 13,
     "image": "treasures/dutch-flag.png",
     "xws": "dutchflag"
   },
   {
     "name": "English Flag",
-    "points": 15,
+    "points": 14,
     "image": "treasures/english-flag.png",
     "xws": "englishflag"
   },
   {
     "name": "French Flag",
-    "points": 16,
+    "points": 15,
     "image": "treasures/french-flag.png",
     "xws": "frenchflag"
   },
   {
     "name": "Hardwood Harpoon",
-    "points": 17,
+    "points": 16,
     "image": "treasures/hardwood-harpoon.png",
     "xws": "hardwoodharpoon"
   },
   {
     "name": "Kraken Ring",
-    "points": 18,
+    "points": 17,
     "image": "treasures/kraken-ring.png",
     "xws": "krakenring"
   },
   {
     "name": "Lost Best Friend",
-    "points": 19,
+    "points": 18,
     "image": "treasures/lost-best-friend.png",
     "xws": "lostbestfriend"
   },
   {
     "name": "Lost Daughter",
-    "points": 20,
+    "points": 19,
     "image": "treasures/lost-daughter.png",
     "xws": "lostdaughter"
   },
   {
     "name": "Lost Son",
-    "points": 21,
+    "points": 20,
     "image": "treasures/lost-son.png",
     "xws": "lostson"
   },
   {
     "name": "Lucky Earring",
-    "points": 22,
+    "points": 21,
     "image": "treasures/lucky-earring.png",
     "xws": "luckyearring"
   },
   {
     "name": "One Man's Trash",
-    "points": 23,
+    "points": 22,
     "image": "treasures/one-mans-trash.png",
     "xws": "onemanstrash"
   },
   {
     "name": "Polished Plank",
-    "points": 24,
+    "points": 23,
     "image": "treasures/polished-plank.png",
     "xws": "polishedplank"
   },
   {
     "name": "Rackham's Memoir",
-    "points": 25,
+    "points": 24,
     "image": "treasures/rackhams-memoir.png",
     "xws": "rackhamsmemoir"
   },
   {
     "name": "Raleigh's Spyglass",
-    "points": 26,
+    "points": 25,
     "image": "treasures/raleighs-spyglass.png",
     "xws": "rayleighsspyglass"
   },
   {
     "name": "Royal Signet Ring",
-    "points": 27,
+    "points": 26,
     "image": "treasures/royal-signet-ring.png",
     "xws": "royalsignetring"
   },
   {
     "name": "Ship In A Bottle",
-    "points": 28,
+    "points": 27,
     "image": "treasures/ship-in-a-bottle.png",
     "xws": "shipinabottle"
   },
   {
     "name": "Shipwright's Tools",
-    "points": 29,
+    "points": 28,
     "image": "treasures/shipwrights-tools.png",
     "xws": "shipwrightstools"
   },
   {
     "name": "Silver Chandelier",
-    "points": 30,
+    "points": 29,
     "image": "treasures/silver-chandelier.png",
     "xws": "silverchandelier"
   },
   {
     "name": "Spanish Flag",
-    "points": 31,
+    "points": 30,
     "image": "treasures/spanish-flag.png",
     "xws": "spanishflag"
   },
   {
     "name": "Sweat Of The Sun",
-    "points": 32,
+    "points": 31,
     "image": "treasures/sweat-of-the-sun.png",
     "xws": "sweatofthesun"
   },
   {
     "name": "Tears Of The Moon",
-    "points": 33,
+    "points": 32,
     "image": "treasures/tears-of-the-moon.png",
     "xws": "tearsofthemoon"
   },
   {
     "name": "The Monkey's Paw",
-    "points": 34,
+    "points": 33,
     "image": "treasures/the-monkeys-paw.png",
     "xws": "themonkeyspaw"
   },
   {
     "name": "The Pirate Code",
-    "points": 35,
+    "points": 34,
     "image": "treasures/the-pirate-code.png",
     "xws": "thepiratecode"
   },
   {
     "name": "Treasure Map",
-    "points": 36,
+    "points": 35,
     "image": "treasures/treasure-map.png",
     "xws": "treasuremap"
   },
   {
     "name": "Velvet Eye Patch",
-    "points": 37,
+    "points": 36,
     "image": "treasures/velvet-eye-patch.png",
     "xws": "velveteyepatch"
   }
