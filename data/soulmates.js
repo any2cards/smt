ca2e77c5b991713c9575e43c61c85b95e1eecd9a
any2cards <any2cards@yahoo.com>
2021-07-19 11:58:42 -0700

    Implement Soulmates Tokens

--- data/soulmates.js
+++ data/soulmates.js
@@ -2,55 +2,55 @@
   {
     "name": "Soulmate Crew",
     "points": 0,
-    "image": "soulmates/soulamte-crew-back.png",
+    "image": "soulmates/soulmate-crew-back.png",
     "xws": "soulmatecrew"
   },
   {
     "name": "Soulmate Crew",
     "points": 1,
-    "image": "soulmates/soulamte-crew-front.png",
+    "image": "soulmates/soulmate-crew-front.png",
     "xws": "soulmatecrew"
   },
   {
     "name": "Soulmate Mission",
     "points": 2,
-    "image": "soulmates/soulamte-mission-back.png",
+    "image": "soulmates/soulmate-mission-back.png",
     "xws": "soulmatemission"
   },
   {
     "name": "Soulmate Mission",
     "points": 3,
-    "image": "soulmates/soulamte-mission-front.png",
+    "image": "soulmates/soulmate-mission-front.png",
     "xws": "soulmatemission"
   },
   {
     "name": "Soulmate Ritual",
     "points": 4,
-    "image": "soulmates/soulamte-ritual-back.png",
+    "image": "soulmates/soulmate-ritual-back.png",
     "xws": "soulmateritual"
   },
   {
     "name": "Soulmate Ritual",
     "points": 5,
-    "image": "soulmates/soulamte-ritual-front.png",
+    "image": "soulmates/soulmate-ritual-front.png",
     "xws": "soulmateritual"
   },
   {
     "name": "Soulmate Treasure",
     "points": 6,
-    "image": "soulmates/soulamte-treasure-back.png",
+    "image": "soulmates/soulmate-treasure-back.png",
     "xws": "soulmatetreasure"
   },
   {
     "name": "Soulmate Treasure",
     "points": 7,
-    "image": "soulmates/soulamte-treasure-front.png",
+    "image": "soulmates/soulmate-treasure-front.png",
     "xws": "soulmatetreasure"
   },
   {
     "name": "Soulmates",
     "points": 8,
-    "image": "soulmates/soulamtes.png",
+    "image": "soulmates/soulmates.png",
     "xws": "soulmates"
   }
 ]
