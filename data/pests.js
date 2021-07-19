8cc320c568dec138f206b4ecb375299b84d55104
any2cards <any2cards@yahoo.com>
2021-07-19 11:04:26 -0700

    Implement Pests Tokens

---
+++ data/pests.js
@@ -0,0 +1,14 @@
+[
+  {
+    "name": "Harbor Open",
+    "points": 0,
+    "image": "pests/harbor-open.png",
+    "xws": "harboropen"
+  },
+  {
+    "name": "Stop!",
+    "points": 1,
+    "image": "pests/stop!.png",
+    "xws": "stop!"
+  }
+]
