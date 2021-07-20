953dbe0c4a479ec9ae763d67f8a0a59c6d4e51cd
any2cards <any2cards@yahoo.com>
2021-07-19 17:05:33 -0700

    Implement Monsters Tokens

--- data/monsters.js
+++ data/monsters.js
@@ -1,14 +1,68 @@
 [
   {
-    "name": "The Kraken",
+    "name": "Monster Markers",
     "points": 0,
+    "image": "monsters/monster-markers.png",
+    "xws": "monstermarkers"
+  },
+  {
+    "name": "The Kraken",
+    "points": 1,
     "image": "monsters/the-kraken.png",
     "xws": "thekracken"
   },
   {
+    "name": "The Kraken Back",
+    "points": 2,
+    "image": "monster-markers/the-kraken-back.png",
+    "xws": "thekrackenback"
+  },
+  {
+    "name": "The Kraken Cannon",
+    "points": 3,
+    "image": "monster-markers/the-kraken-cannon.png",
+    "xws": "thekrackencannon"
+  },
+  {
+    "name": "The Kraken Gold",
+    "points": 4,
+    "image": "monster-markers/the-kraken-gold.png",
+    "xws": "thekrackengold"
+  },
+  {
+    "name": "The Kraken Treasure",
+    "points": 5,
+    "image": "monster-markers/the-kraken-treasure.png",
+    "xws": "thekrackentreasure"
+  },
+  {
     "name": "The Sea Serpent",
-    "points": 1,
+    "points": 6,
     "image": "monsters/the-sea-serpent.png",
     "xws": "theseaserpent"
+  },
+  {
+    "name": "The Sea Serpent Back",
+    "points": 7,
+    "image": "monster-markers/the-sea-serpent-back.png",
+    "xws": "theseaserpentback"
+  },
+  {
+    "name": "The Sea Serpent Cannon",
+    "points": 8,
+    "image": "monster-markers/the-sea-serpent-cannon.png",
+    "xws": "theseaserpentcannon"
+  },
+  {
+    "name": "The Sea Serpent Gold",
+    "points": 9,
+    "image": "monster-markers/the-sea-serpent-gold.png",
+    "xws": "theseaserpentgold"
+  },
+  {
+    "name": "The Sea Serpent Treasure",
+    "points": 10,
+    "image": "monster-markers/the-sea-serpent-treasure.png",
+    "xws": "theseaserpentreasure"
   }
 ]
