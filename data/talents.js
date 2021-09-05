3dd66aa6560f8e5f2d3bfc433e5561ddc90a0bd7
any2cards <any2cards@yahoo.com>
2021-09-05 16:43:00 -0700

    Implement Black Talent

--- data/talents.js
+++ data/talents.js
@@ -1,61 +1,67 @@
 [
   {
-    "name": "blue talent back",
+    "name": "black talent back",
     "points": 0,
+    "image": "talents/black-talent-back.png",
+    "xws": "blacktalentback"
+  },
+  {
+    "name": "blue talent back",
+    "points": 1,
     "image": "talents/blue-talent-back.png",
     "xws": "bluetalentback"
   },
   {
     "name": "green talent back",
-    "points": 1,
+    "points": 2,
     "image": "talents/green-talent-back.png",
     "xws": "greentalentback"
   },
   {
     "name": "red talent back",
-    "points": 2,
+    "points": 3,
     "image": "talents/red-talent-back.png",
     "xws": "redtalentback"
   },
   {
     "name": "yellow talent back",
-    "points": 3,
+    "points": 4,
     "image": "talents/yellow-talent-back.png",
     "xws": "yellowtalentback"
   },
   {
     "name": "haggler",
-    "points": 4,
+    "points": 5,
     "image": "talents/haggler.png",
     "xws": "haggler"
   },
   {
     "name": "hunter",
-    "points": 5,
+    "points": 6,
     "image": "talents/hunter.png",
     "xws": "hunter"
   },
   {
     "name": "keen eyes",
-    "points": 6,
+    "points": 7,
     "image": "talents/keen-eyes.png",
     "xws": "keeneyes"
   },
   {
     "name": "navigator",
-    "points": 7,
+    "points": 8,
     "image": "talents/navigator.png",
     "xws": "navigator"
   },
   {
     "name": "ruffian",
-    "points": 8,
+    "points": 9,
     "image": "talents/ruffian.png",
     "xws": "ruffian"
   },
   {
     "name": "tramp",
-    "points": 9,
+    "points": 10,
     "image": "talents/tramp.png",
     "xws": "tramp"
   }
