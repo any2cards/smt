3ce4e66c7bf36bd0920c51d98de00eeed0246f42
any2cards <any2cards@yahoo.com>
2021-09-05 08:42:07 -0700

    Implement Merchants

--- data/merchants.js
+++ data/merchants.js
@@ -2,12 +2,6 @@
   {
     "name": "large blue merchant",
     "points": 0,
-    "image": "merchants/large-blue-back.png",
-    "xws": "largebluemerchant"
-  },
-  {
-    "name": "large blue merchant",
-    "points": 1,
     "image": "merchants/large-blue-4-5-6-front.png",
     "xws": "largebluemerchant"
   }
