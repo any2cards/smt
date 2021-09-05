9c783b26d4ff3337408b42b28126a8c8c22fb2c8
any2cards <any2cards@yahoo.com>
2021-09-05 09:32:21 -0700

    Implement Merchants

--- data/merchants.js
+++ data/merchants.js
@@ -7,20 +7,44 @@
   },
   {
     "name": "large orange merchant",
-    "points": 4,
+    "points": 1,
     "image": "merchants/large-orange-merchant.png",
     "xws": "largeorangemerchant"
   },
   {
     "name": "large white merchant",
-    "points": 8,
+    "points": 2,
     "image": "merchants/large-white-merchant.png",
     "xws": "largewhitemerchant"
   },
   {
     "name": "large yellow merchant",
-    "points": 11,
+    "points": 3,
     "image": "merchants/large-yellow-merchant.png",
     "xws": "largeyellowmerchant"
+  },
+  {
+    "name": "small blue merchant",
+    "points": 4,
+    "image": "merchants/small-blue-merchant.png",
+    "xws": "smallbluemerchant"
+  },
+  {
+    "name": "small orange merchant",
+    "points": 5,
+    "image": "merchants/small-orange-merchant.png",
+    "xws": "smallorangemerchant"
+  },
+  {
+    "name": "small white merchant",
+    "points": 6,
+    "image": "merchants/small-white-merchant.png",
+    "xws": "smallwhitemerchant"
+  },
+  {
+    "name": "small yellow merchant",
+    "points": 7,
+    "image": "merchants/small-yellow-merchant.png",
+    "xws": "smallyellowmerchant"
   }
 ]
