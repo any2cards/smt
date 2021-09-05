2f0c7f47689fca63ca4347305b153a5bea5eee37
any2cards <any2cards@yahoo.com>
2021-09-05 16:36:24 -0700

    Implement Black Origin

--- data/origins.js
+++ data/origins.js
@@ -1,61 +1,67 @@
 [
   {
-    "name": "blue origin back",
+    "name": "black origin back",
     "points": 0,
+    "image": "origins/black-origin-back.png",
+    "xws": "blackoriginback"
+  },
+  {
+    "name": "blue origin back",
+    "points": 1,
     "image": "origins/blue-origin-back.png",
     "xws": "blueoriginback"
   },
   {
     "name": "green origin back",
-    "points": 1,
+    "points": 2,
     "image": "origins/green-origin-back.png",
     "xws": "greenoriginback"
   },
   {
     "name": "red origin back",
-    "points": 2,
+    "points": 3,
     "image": "origins/red-origin-back.png",
     "xws": "redoriginback"
   },
   {
     "name": "yellow origin back",
-    "points": 3,
+    "points": 4,
     "image": "origins/yellow-origin-back.png",
     "xws": "yelloworiginback"
   },
   {
     "name": "isla oscura",
-    "points": 4,
+    "points": 5,
     "image": "origins/isla-oscura-origin.png",
     "xws": "islaoscura"
   },
   {
     "name": "la juanita",
-    "points": 5,
+    "points": 6,
     "image": "origins/la-juanita-origin.png",
     "xws": "lajuanita"
   },
   {
     "name": "port royal",
-    "points": 6,
+    "points": 7,
     "image": "origins/port-royal-origin.png",
     "xws": "portroyal"
   },
   {
     "name": "sao tome",
-    "points": 7,
+    "points": 8,
     "image": "origins/sao-tome-origin.png",
     "xws": "saotome"
   },
   {
     "name": "st. david",
-    "points": 8,
+    "points": 9,
     "image": "origins/st-david-origin.png",
     "xws": "stdavid"
   },
   {
     "name": "the bay",
-    "points": 9,
+    "points": 10,
     "image": "origins/the-bay-origin.png",
     "xws": "thebay"
   }
