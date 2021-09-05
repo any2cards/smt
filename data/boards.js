721a4f8eebb10a3a261cd7d840245e0be1244198
any2cards <any2cards@yahoo.com>
2021-09-05 12:46:12 -0700

    Implement Boards

--- data/boards.js
+++ data/boards.js
@@ -6,9 +6,39 @@
     "xws": "epicgameboard"
   },
   {
-    "name": "standard game board",
+    "name": "player mat",
     "points": 1,
+    "image": "boards/player-mat.png",
+    "xws": "playermat"
+  },
+  {
+    "name": "standard game board",
+    "points": 2,
     "image": "boards/standard-game-board.png",
     "xws": "standardgameboard"
+  },
+  {
+    "name": "treasure map 1",
+    "points": 3,
+    "image": "boards/treasure-map-1.png",
+    "xws": "treasuremap1"
+  },
+  {
+    "name": "treasure map 2",
+    "points": 4,
+    "image": "boards/treasure-map-2.png",
+    "xws": "treasuremap2"
+  },
+  {
+    "name": "treasure map 3",
+    "points": 5,
+    "image": "boards/treasure-map-3.png",
+    "xws": "treasuremap3"
+  },
+  {
+    "name": "treasure map 4",
+    "points": 6,
+    "image": "boards/treasure-map-4.png",
+    "xws": "treasuremap4"
   }
 ]
