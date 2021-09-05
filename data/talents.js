56a8f3914e21cedde5c66f3679bf35016c4914c6
any2cards <any2cards@yahoo.com>
2021-09-05 12:36:58 -0700

    Implement Talents

--- data/talents.js
+++ data/talents.js
@@ -6,38 +6,56 @@
     "xws": "bluetalentback"
   },
   {
-    "name": "haggler",
+    "name": "green talent back",
     "points": 1,
+    "image": "talents/green-talent-back.png",
+    "xws": "greentalentback"
+  },
+  {
+    "name": "red talent back",
+    "points": 2,
+    "image": "talents/red-talent-back.png",
+    "xws": "redtalentback"
+  },
+  {
+    "name": "yellow talent back",
+    "points": 3,
+    "image": "talents/yellow-talent-back.png",
+    "xws": "yellowtalentback"
+  },
+  {
+    "name": "haggler",
+    "points": 4,
     "image": "talents/haggler.png",
     "xws": "haggler"
   },
   {
     "name": "hunter",
-    "points": 2,
+    "points": 5,
     "image": "talents/hunter.png",
     "xws": "hunter"
   },
   {
     "name": "keen eyes",
-    "points": 3,
+    "points": 6,
     "image": "talents/keen-eyes.png",
     "xws": "keeneyes"
   },
   {
     "name": "navigator",
-    "points": 4,
+    "points": 7,
     "image": "talents/navigator.png",
     "xws": "navigator"
   },
   {
     "name": "ruffian",
-    "points": 5,
+    "points": 8,
     "image": "talents/ruffian.png",
     "xws": "ruffian"
   },
   {
     "name": "tramp",
-    "points": 6,
+    "points": 9,
     "image": "talents/tramp.png",
     "xws": "tramp"
   }
