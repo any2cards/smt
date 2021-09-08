e5f39b67f1211c6398cb36015bbc7f16dc31109c
any2cards <any2cards@yahoo.com>
2021-09-08 14:48:43 -0700

    Implement Miniatures

--- data/miniatures.js
+++ data/miniatures.js
@@ -1,8 +1,92 @@
 [
   {
-    "name": "duel miniature",
+    "name": "black cannon",
     "points": 0,
+    "image": "miniatures/black-cannon.png",
+    "xws": "blackcannon"
+  },
+  {
+    "name": "blue cannon",
+    "points": 1,
+    "image": "miniatures/blue-cannon.png",
+    "xws": "bluecannon"
+  },
+  {
+    "name": "green cannon",
+    "points": 2,
+    "image": "miniatures/green-cannon.png",
+    "xws": "greencannon"
+  },
+  {
+    "name": "red cannon",
+    "points": 3,
+    "image": "miniatures/red-cannon.png",
+    "xws": "redcannon"
+  },
+  {
+    "name": "yellow cannon",
+    "points": 4,
+    "image": "miniatures/yellow-cannon.png",
+    "xws": "yellowcannon"
+  },
+  {
+    "name": "black sails",
+    "points": 5,
+    "image": "miniatures/black-sails.png",
+    "xws": "blacksails"
+  },
+  {
+    "name": "blue sails",
+    "points": 6,
+    "image": "miniatures/blue-sails.png",
+    "xws": "bluesails"
+  },
+  {
+    "name": "green sails",
+    "points": 7,
+    "image": "miniatures/green-sails.png",
+    "xws": "greensails"
+  },
+  {
+    "name": "red sails",
+    "points": 8,
+    "image": "miniatures/red-sails.png",
+    "xws": "redsails"
+  },
+  {
+    "name": "yellow sails",
+    "points": 9,
+    "image": "miniatures/yellow-sails.png",
+    "xws": "yellowsails"
+  },
+  {
+    "name": "cargo",
+    "points": 10,
+    "image": "miniatures/cargo.png",
+    "xws": "cargo"
+  },
+  {
+    "name": "chest",
+    "points": 11,
+    "image": "miniatures/chest.png",
+    "xws": "chest"
+  },
+  {
+    "name": "duel miniature",
+    "points": 12,
     "image": "miniatures/duel-miniature.png",
     "xws": "duelminiature"
+  },
+  {
+    "name": "ship",
+    "points": 13,
+    "image": "miniatures/ship-top.png",
+    "xws": "ship"
+  },
+  {
+    "name": "ship",
+    "points": 14,
+    "image": "miniatures/ship-side.png",
+    "xws": "ship"
   }
 ]
